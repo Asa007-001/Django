0538138d673b5c771816b5a91afa04dcc3c51b7b
Asa007-001 <may931116@gmail.com>
2024-11-19 21:32:15 +0800

    Asa007-001

--- appname/admin.py
+++ appname/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from appname.models import I
 
-# Register your models here.
+class IAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publisher', 'publication_date', 'price')
+    list_filter = ('author', 'publisher')
+    search_fields = ('title',)
+    ordering = ('publication_date',)
+
+admin.site.register(I, IAdmin)
--- appname/models.py
+++ appname/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class I(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    publication_date = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    bookname = models.CharField(max_length=200)  # 假設你有一個 bookname 欄位
+
+    def __str__(self):
+        return self.bookname + '  ' + self.author
